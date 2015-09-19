f0ae1d08bf79876e61cfe2d758bb80c521803efd
Dan DePalma <ddepalma0@gmail.com>
2015-09-19 16:22:08 -0400

    navbar partial idea link

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,7 +1,25 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Dan
- * Date: 9/19/2015
- * Time: 10:59 AM
- */
+<!-- Static navbar -->
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">IdeaLikes</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="/">Home</a></li>
+                <li><a href="/ideas">Ideas</a></li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li> {!! link_to_action('IdeasController@show', $latest->ideaname, [$latest->id]) !!}</li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div><!--/.container-fluid -->
+</nav>
