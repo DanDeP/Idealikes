ef1bdff4af1d4e93be4775e3f94396ac427a827d
Dan DePalma <ddepalma0@gmail.com>
2015-09-21 21:35:08 -0400

    Dropdown menu sorted

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Ideas;
+use App\Tag;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->composer('partials.nav', function($view)
+        {
+            //$tags = Tag::all()->get();
+
+            $view->with('latest', Ideas::latest()->first())
+                ->with('categories', Tag::orderBy('name')->get());
+        });
     }
 
     /**
