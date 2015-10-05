21d67881974b63c30de9e8c87decb778d4d8e613
Dan DePalma <ddepalma0@gmail.com>
2015-10-05 15:59:56 -0400

    notsure

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,4 +1,7 @@
-<!-- Static navbar -->
+@if (Auth::check())
+    <script type="text/javascript">
+
+    </script>
 <nav class="navbar navbar-inverse">
     <div class="container-fluid">
         <div class="navbar-header">
@@ -8,18 +11,37 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">IdeaLikes</a>
+            <a class="navbar-brand">IdeaLikes</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
 
             <ul class="nav navbar-nav">
-                <li class="active"><a href="/">Home</a></li>
-                <li><a href="/ideas">Ideas</a></li>
+                <li class="active"><a href="/ideas">Ideas</a></li>
+                <li><a href="/ideas">Likes</a></li>
+            </ul>
+
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Test <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        @foreach($categories as $category)
+                            <li><a href="/tags/{{$category->name}}">{{ $category->name }}</a></li>
+                        @endforeach
+                    </ul>
+                </li>
             </ul>
 
             <ul class="nav navbar-nav navbar-right">
-                <li> {!! link_to_action('IdeasController@show', $latest->ideaname, [$latest->id]) !!}</li>
+                    <li><a class="try"> Welcome  {!! Auth::user()->username !!}</a></li>
+                    <li @if (Request::is('auth/login/post*')) class="active" @endif>
+                        <a href="/auth/logout">Logout</a>
+                    </li>
+
             </ul>
+            <!--<ul class="nav navbar-nav navbar-right">
+                <li> {!! link_to_action('IdeasController@show', $latest->ideaname, [$latest->id]) !!}</li>
+            </ul>-->
         </div><!--/.nav-collapse -->
     </div><!--/.container-fluid -->
 </nav>
+@endif
