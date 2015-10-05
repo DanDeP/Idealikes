8b76d00964fdc4e6be36715da85d2ee67fc19182
Dan DePalma <ddepalma0@gmail.com>
2015-10-05 15:58:00 -0400

    query to get and list ideas added

--- app/Users.php
+++ app/Users.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class Users extends Model implements AuthenticatableContract, CanResetPasswordContract
 {
@@ -24,9 +26,9 @@ class Users extends Model implements AuthenticatableContract, CanResetPasswordCo
 
     protected $dates = ['datejoined'];
 
-   public function setDateCreated($date)
+    public function setDateCreated($date)
     {
-        $this->attributes['datejoined']=$date;
+        $this->attributes['datejoined'] = $date;
     }
 
     //A user can have many ideas
@@ -35,4 +37,20 @@ class Users extends Model implements AuthenticatableContract, CanResetPasswordCo
         return $this->hasMany('App\Ideas');
     }
 
+    protected $hidden = ['password', 'remember_token'];
+
+    public static function getUnratedIdea()
+    {
+        $idea = DB::table('ideas')
+            ->select('ideas.id', 'ideas.ideaname', 'ideas.idea')
+            ->where('ideas.users_id', '!=', Auth::id())
+            ->whereNotIn('ideas.id', function ($query) {
+                $query->select('likes.idea_id')
+                    ->from('likes')
+                    ->where('user_id', Auth::id());
+            })
+            ->get();
+
+        return $idea;
+    }
 }
