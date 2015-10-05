f61f20332e5645c1e346850732383c5856566f2c
Dan DePalma <ddepalma0@gmail.com>
2015-10-05 15:57:36 -0400

    insert into likes table the like or dislike

--- app/Likes.php
+++ app/Likes.php
@@ -3,8 +3,36 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Likes extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'idea_id',
+        'is_liked',
+        'is_disliked'
+    ];
+    public function setDateCreated($date)
+    {
+        $this->attributes['dateuploaded']=$date;
+    }
+    /**
+     * This will insert a record into the likes table
+     */
+    public static function isRated($idea_id,$user_id,$rate){
+        //var $is_liked = $rate;
+        if($rate == 'Like'){
+            $is_liked = 'is_liked';
+        }else{
+            $is_liked = 'is_disliked';
+        }
+
+        $isRated = DB::table('likes')
+            ->insert(
+                array('user_id' => $user_id,
+                      'idea_id' => $idea_id,
+                      $is_liked => 1)
+            );
+    }
 }
