7837075f9115fb7e83099759cbcae38864a58036
Dan DePalma <ddepalma0@gmail.com>
2015-10-07 16:21:39 -0400

    updated tags controller

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use App\Tag;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Users;
 
 class TagsController extends Controller
 {
     public function show(Tag $tag)
     {
-
+        $unratedIdeas = Users::getUnratedIdea();
         $ideas = $tag->ideas;
-        return view('Ideas.index', compact('ideas'));
+        return view('Ideas.index', compact('ideas','unratedIdeas'));
     }
 }
