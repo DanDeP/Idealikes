7c4edbcf18145245b585e8226e572df5c5225d7a
Dan DePalma <ddepalma0@gmail.com>
2015-10-17 12:29:58 -0400

    MyIdeasController

--- app/Http/Controllers/MyIdeasController.php
+++ app/Http/Controllers/MyIdeasController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-class MyIdeas extends Controller
+class MyIdeasController extends Controller
 {
     //
 }
