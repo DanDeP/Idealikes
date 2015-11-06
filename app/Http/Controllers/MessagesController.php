a61c82306558ef31b625e441f1dc099907a0879d
Dan DePalma <ddepalma0@gmail.com>
2015-11-06 00:00:46 -0500

    messages controller

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -26,9 +26,9 @@ class MessagesController extends Controller
     {
         $currentUserId = Auth::user()->id;
         // All threads, ignore deleted/archived participants
-        $threads = Thread::getAllLatest()->get();
+        //$threads = Thread::getAllLatest()->get();
         // All threads that user is participating in
-        // $threads = Thread::forUser($currentUserId)->latest('updated_at')->get();
+         $threads = Thread::forUser($currentUserId)->latest('updated_at')->get();
         // All threads that user is participating in, with new messages
         // $threads = Thread::forUserWithNewMessages($currentUserId)->latest('updated_at')->get();
         return view('messenger.index', compact('threads', 'currentUserId'));
@@ -60,10 +60,10 @@ class MessagesController extends Controller
      *
      * @return mixed
      */
-    public function create()
+    public function create($id)
     {
         $users = Users::where('id', '!=', Auth::id())->get();
-        return view('messenger.create', compact('users'));
+        return view('messenger.create', compact('users','id'));
     }
     /**
      * Stores a new message thread
@@ -94,7 +94,7 @@ class MessagesController extends Controller
                 'last_read' => new Carbon
             ]
         );
-        // Recipients
+        // Recipients change the check boxes to hidden field in form which contains sender and sendee
         if (Input::has('recipients')) {
             $thread->addParticipants($input['recipients']);
         }
