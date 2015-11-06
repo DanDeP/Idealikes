93bbfd2de9f444de23cc2f8930515ef4411f4c53
Dan DePalma <ddepalma0@gmail.com>
2015-11-05 23:59:03 -0500

    index messenger blade

--- resources/views/messenger/index.blade.php
+++ resources/views/messenger/index.blade.php
@@ -8,13 +8,17 @@
     @endif
     @if($threads->count() > 0)
         @foreach($threads as $thread)
-            <?php $class = $thread->isUnread($currentUserId) ? 'alert-info' : ''; ?>
-            <div class="media alert {!!$class!!}">
-                <h4 class="media-heading">{!! link_to('messages/' . $thread->id, $thread->subject) !!}</h4>
-                <p>{!! $thread->latestMessage->body !!}</p>
-                <p><small><strong>Creator:</strong> {!! $thread->creator()->name !!}</small></p>
-                <p><small><strong>Participants:</strong> {!! $thread->participantsString(Auth::id()) !!}</small></p>
-            </div>
+
+            {{--@if($thread->participantsUserIds()[0] == Auth::id() || $thread->participantsUserIds()[1] == Auth::id())--}}
+
+                <?php $class = $thread->isUnread($currentUserId) ? 'alert-info' : ''; ?>
+                <div class=" {!!$class!!}"><!--media alert-->
+                    <h4 class="media-heading">{!! link_to('messages/' . $thread->id, $thread->subject) !!}</h4>
+                    <p>{!! $thread->latestMessage->body !!}</p>
+                    <p><small><strong>Creator:</strong> {!! $thread->creator()->username !!}</small></p>
+                    <p><small><strong>Participants:</strong> {!! $thread->participantsString(Auth::id()) !!}</small></p>
+                </div>
+            {{--@endif--}}
         @endforeach
     @else
         <p>Sorry, no threads.</p>
