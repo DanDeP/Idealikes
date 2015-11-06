23140177146d0489bbedd7da9b6196e83de657ab
Dan DePalma <ddepalma0@gmail.com>
2015-11-05 23:59:16 -0500

    create message blade

--- resources/views/messenger/create.blade.php
+++ resources/views/messenger/create.blade.php
@@ -1,7 +1,7 @@
 @extends('app')
 
 @section('content')
-    <h1>Create a new message</h1>
+    <h1>Send Message to:</h1>
     {!! Form::open(['route' => 'messages.store']) !!}
     <div class="col-md-6">
         <!-- Subject Form Input -->
@@ -14,15 +14,16 @@
         <div class="form-group">
             {!! Form::label('message', 'Message', ['class' => 'control-label']) !!}
             {!! Form::textarea('message', null, ['class' => 'form-control']) !!}
+            <input type="hidden" name="recipients[]" value="{{$id}}"/>
         </div>
 
-        @if($users->count() > 0)
-            <div class="checkbox">
-                @foreach($users as $user)
-                    <label title="{!!$user->name!!}"><input type="checkbox" name="recipients[]" value="{!!$user->id!!}">{!!$user->name!!}</label>
-                @endforeach
-            </div>
-            @endif
+        {{--@if($users->count() > 0)--}}
+            {{--<div class="checkbox">--}}
+                {{--@foreach($users as $user)--}}
+                    {{--<label title="{!!$user->username!!}"><input type="checkbox" name="recipients[]" value="{!!$user->id!!}">{!!$user->username!!}</label>--}}
+                {{--@endforeach--}}
+            {{--</div>--}}
+            {{--@endif--}}
 
                     <!-- Submit Form Input -->
             <div class="form-group">
